d89bf1de4507580362f3a4c138eec8d20f40f449
Nguyen Duy Quang <quangndgcc200030@gmail.com>
2024-05-10 00:03:10 +0700

    refactor

--- src/app/services/ws/stomp.service.ts
+++ src/app/services/ws/stomp.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as SocketJs from 'sockjs-client';
 import * as Stomp from 'stompjs';
+import { AuthService } from '../auth.service';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +9,8 @@ import * as Stomp from 'stompjs';
 export class StompService {
   socket = new SocketJs('/ws');
   stompClient = Stomp.over(this.socket);
+
+  constructor(private authService: AuthService) {}
 
   subscribe(topic: string, callback: (message: any) => void): void {
     const connected: boolean = this.stompClient.connected;
@@ -17,9 +20,16 @@ export class StompService {
       return;
     }
 
-    this.stompClient.connect({}, () => {
-      this.subscribeToTopic(topic, callback);
-    });
+    this.stompClient.connect(
+      {
+        Headers: {
+          Authorization: `Bearer ${this.authService.getAccessToken()}`,
+        },
+      },
+      () => {
+        this.subscribeToTopic(topic, callback);
+      }
+    );
   }
 
   private subscribeToTopic(
